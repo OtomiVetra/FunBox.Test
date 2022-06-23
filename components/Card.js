67f68299af87a539cc74c066bebd1a726408d5de
OtomiVetra <otomivetra@mail.ru>
2022-06-23 05:36:06 +0300

    add_dynamics

--- components/Card.js
+++ components/Card.js
@@ -1,12 +1,20 @@
-const Card = () => {
+const Card = ({ card, onClick }) => {
+  const { ingred, active } = card;
+  const cardClasses = ['card'];
+  if (active) {
+    cardClasses.push('card_active');
+  }
   return (
-    <div className="card">
-      <p className="card__text">Сказочное заморское явство</p>
-      <h3 className="card__product-name">Нямушка</h3>
-      <p className="card__product-ingred">c фуа-гра</p>
-      <p className="card__product-amount">10 порций</p>
-      <p className="card__product-stock">мышь в подарок</p>
-      <div className="card__circle"></div>
+    <div className={cardClasses.join(' ')} onClick={onClick}>
+      <img className="card__bg" src="/image/card-bg.png" />
+      <div className="card__content">
+        <p className="card__text">Сказочное заморское явство</p>
+        <h3 className="card__product-name">Нямушка</h3>
+        <p className="card__product-ingred">{ingred}</p>
+        <p className="card__product-amount">10 порций</p>
+        <p className="card__product-stock">мышь в подарок</p>
+        <div className="card__circle"></div>
+      </div>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,38 @@
+import { useState } from 'react';
 import Card from '../components/Card';
 const IndexPage = () => {
+  const [cards, setCards] = useState([
+    {
+      active: true,
+      ingred: 'с фуа-гра',
+    },
+    {
+      active: false,
+      ingred: 'с курой',
+    },
+    {
+      active: false,
+      ingred: 'с фуа-гра',
+    },
+  ]);
+  const handleClick = (i) => {
+    setCards(
+      cards.map((card, j) => {
+        if (i !== j) return card;
+        return { ...card, active: !card.active };
+      })
+    );
+  };
+
   return (
-    <div className="background">
-      <h1 className="header">Ты сегодня покормил кота?</h1>
-      <div className="cards">
-        <Card />
-        <Card />
-        <Card />
+    <div className="background page">
+      <div className="page__content">
+        <h1 className="header">Ты сегодня покормил кота?</h1>
+        <div className="cards">
+          {cards.map((card, i) => {
+            return <Card key={i} card={card} onClick={() => handleClick(i)} />;
+          })}
+        </div>
       </div>
     </div>
   );
